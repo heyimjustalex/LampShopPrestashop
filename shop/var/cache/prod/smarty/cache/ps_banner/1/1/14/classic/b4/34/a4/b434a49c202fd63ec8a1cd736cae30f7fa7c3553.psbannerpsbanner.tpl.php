9c07b6642464a9f965cc1fb87fc3afe32b359c00
TheWinczi <mwinczewski@tutanota.com>
2021-11-02 13:11:13 +0100

    All needed shop file have been created

---
+++ shop/var/cache/prod/smarty/cache/ps_banner/1/1/14/classic/b4/34/a4/b434a49c202fd63ec8a1cd736cae30f7fa7c3553.psbannerpsbanner.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-10-20 16:13:15
+  from 'module:psbannerpsbanner.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_617023fbc3ab72_59740887',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '64034093fbda864710a2c0f526b37f921e53ee39' => 
+    array (
+      0 => 'module:psbannerpsbanner.tpl',
+      1 => 1633363913,
+      2 => 'module',
+    ),
+  ),
+  'cache_lifetime' => 31536000,
+),true)) {
+function content_617023fbc3ab72_59740887 (Smarty_Internal_Template $_smarty_tpl) {
+?><a class="banner" href="http://localhost:8000/" title="">
+      <img src="http://localhost:8000/modules/ps_banner/img/sale70.png" alt="" title="" class="img-fluid" loading="lazy" width="1110" height="213">
+  </a>
+<?php }
+}
---
+++ shop/var/cache/prod/smarty/compile/65/3d/8a/653d8a43204f3a0b8227429d5e019cd630956570_0.file.layout.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-10-20 16:09:51
+  from '/var/www/html/admin742rovy9v/themes/default/template/controllers/login/layout.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_6170232f8432b4_86400816',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '653d8a43204f3a0b8227429d5e019cd630956570' => 
+    array (
+      0 => '/var/www/html/admin742rovy9v/themes/default/template/controllers/login/layout.tpl',
+      1 => 1633363913,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6170232f8432b4_86400816 (Smarty_Internal_Template $_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['header']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['page']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['footer']->value;?>
+
+<?php }
+}
