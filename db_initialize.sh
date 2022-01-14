89d140dbec21d9dec02511453368c4393b938339
heyimjustalex <alex@DESKTOP-PI25JU7.localdomain>
2022-01-14 12:20:42 +0100

    Fix bugs

--- db_initialize.sh
+++ db_initialize.sh
@@ -2,9 +2,9 @@
 
 CONTAINER="admin-mysql_db.1.jhmt04hxq732lvnr76m7ikzlv"
 
-docker cp ./*.sql $CONTAINER:/tmp/BE_176084.sql
+docker cp ./*.sql $CONTAINER:/tmp/DB_176084.sql
 docker cp ./db_setup.sh $CONTAINER:/tmp/db_setup.sh
 docker exec -it $CONTAINER  chmod 777 /tmp/db_setup.sh
 docker exec -it $CONTAINER /bin/sh /tmp/db_setup.sh
-docker exec -it $CONTAINER  rm /tmp/BE_176084.sql
+docker exec -it $CONTAINER  rm /tmp/DB_176084.sql
 docker exec -it $CONTAINER  rm /tmp/db_setup.sh
